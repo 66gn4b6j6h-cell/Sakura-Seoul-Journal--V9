7d0746b5557083e01ac4bbfd0d6ffbd3b6ef92ba
66gn4b6j6h-cell <66gn4b6j6h@privaterelay.appleid.com>
2026-09-19 18:14:39 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-// V9 deliberately does not cache the application shell.
-// This prevents normal Safari from being pinned to an older GitHub Pages build.
-self.addEventListener('install',()=>self.skipWaiting());
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',()=>{});
